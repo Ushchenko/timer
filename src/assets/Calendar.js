36210e3111360c4b38ec5070b352afe1920ed7df
Ushchenko <danyodesa@gmail.com>
2022-12-12 01:23:41 +0200

    ver 0.3

--- src/assets/Calendar.js
+++ src/assets/Calendar.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/calendar.css';
 
 export default Calendar;
 
 
+const nowDate = new Date();
+
+let nowYear = nowDate.getFullYear(),
+	nowMonth = nowDate.getMonth();
+
+
 function Calendar() {
-	const nowDate = new Date(),
-		monthName = ["January", "February", "March",
+	const monthName = ["January", "February", "March",
 			"April", "May", "June", "July",
 			"August", "September", "October",
 			"November", "December"
 		];
 	
-	const [nowMonth, setMonth] = useState(monthName[nowDate.getMonth()]);
-	const [nowYear, setYear] = useState(nowDate.getFullYear());
+	let [nowMonthName, setMonthName] = useState(monthName[nowDate.getMonth()]),
+		[nowYearName, setYearName] = useState(nowDate.getFullYear());
+
+	function handleNowMonthName(newNowMonthName) {
+		setMonthName(newNowMonthName)
+	}
+
+	function handleNowYearName(newNowYearName) {
+		setYearName(newNowYearName)
+	}
 
 	return (
 		<section id="calendar">
@@ -21,6 +34,15 @@ function Calendar() {
 				<ul className="scene__month">
 					<li className='carret prev'>
 						<button onClick={() => {
+							nowMonth--;
+
+							if(nowMonth <= 0) {
+								nowMonth = 11;
+								nowYear--;
+							}
+							handleNowMonthName(monthName[nowMonth]);
+							handleNowYearName(nowYear);
+							Days(nowYear, nowMonth);
 						}}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
 								<path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
@@ -29,12 +51,21 @@ function Calendar() {
 					</li>
 					<li>
 						<div className='month__text'>
-							<span className='month--name'>{nowMonth}</span>
-							<span className='year--name'>{nowYear}</span>
+							<span className='month--name'>{nowMonthName}</span>
+							<span className='year--name'>{nowYearName}</span>
 						</div>
 					</li>
 					<li className='carret next'>
 						<button onClick={() => {
+							nowMonth++;
+
+							if(nowMonth >= 12) {
+								nowMonth = 0;
+								nowYear++;
+							}
+							handleNowMonthName(monthName[nowMonth]);
+							handleNowYearName(nowYear);
+							Days(nowYear, nowMonth)
 						}}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
 								<path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
@@ -52,8 +83,35 @@ function Calendar() {
 					<li>Sun</li>
 				</ul>
 				<ul className="days">
+					{Days(nowYear, nowMonth)}
 				</ul>
 			</div>
 		</section>
+	)
+}
+
+function Days(year, month) {
+
+	const calendarDays = {
+		tag: []
+	};
+	let
+	 	daysAmount = new Date(year, month + 1, 0).getDate(),
+		daysPrefix = new Date(year, month, 0).getDay(),
+		prevMonthsDaysAmount = new Date(year, month, 0).getDate(),
+		prevMonthsPostfix = new Date(year, month + 1, 0).getDay();
+		
+
+	if(daysPrefix > 0) 
+		for(let i = 1, j = prevMonthsDaysAmount - daysPrefix + 1; i <= daysPrefix; i++, j++) 
+			calendarDays.tag.push(<li className='days--number--prev'>{j}</li>)
+		
+
+	for(let i = 1; i <= daysAmount; i++)
+		calendarDays.tag.push(<li className='days--number'>{i}</li>)
+
+	
+	return (
+		calendarDays.tag
 	)
 }
